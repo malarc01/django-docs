84a78be80f84e318f79cc237eafa914f6692502e
malarc <email@email.com>
2020-11-04 12:57:49 -0500

    refactored detail view to use Http404 & render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.http import Http404
 
 from .models import Question
 
@@ -14,7 +15,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, 'polls/details.html', {question: 'question'})
 
 
 def results(request, question_id):
