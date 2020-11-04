6b793aceafb5fcedee4c2d1c6e1cca0fe5a9837f
malarc <email@email.com>
2020-11-04 13:23:31 -0500

    add /specifics/ to urlpattens

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: HOST/polls/5/
     # the 'name' value is called by the {% url %} template tag
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('specific/<int:question_id>/', views.detail, name='detail'),
     # ex: HOST/polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: HOST/polls/5/vote/
