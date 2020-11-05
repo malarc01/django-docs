803e9a4f5a3f776c5d9c8c355c90eb1b52942a60
malarc <email@email.com>
2020-11-05 12:41:51 -0500

    add URLconf

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,12 +6,12 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: HOST/polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: HOST/polls/5/
     # the 'name' value is called by the {% url %} template tag
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: HOST/polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: HOST/polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
